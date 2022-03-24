3f0f75f4c687db177d82b04b4dcb74e07a5ef84a
dongyang <yyancy517@gmail.com>
2022-03-24 15:01:50 +0800

    changes

---
+++ server/InMemory_test.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCutLast(t *testing.T) {
+	buf := []byte("100\n200\n300\n10")
+	wantRes, wantRest := []byte("100\n200\n300\n"), []byte("10")
+	gotRes, gotRest, err := cutLast(buf)
+	if err != nil {
+		t.Errorf("cutLast(%v): got error %v; want no error", buf, err)
+	}
+
+	if !bytes.Equal(wantRes, gotRes) || !bytes.Equal(wantRest, gotRest) {
+		t.Errorf("cutLast(%q): got %q, %q; want %q, %q",
+			string(buf), string(gotRes), string(gotRest), string(wantRes), string(wantRest))
+	}
+
+}
+func TestCutLastErrors(t *testing.T) {
+	buf := []byte("100")
+	_, _, err := cutLast(buf)
+	if err == nil {
+		t.Errorf("TestCutLastErrors(%q): want error; but no error", string(buf))
+	}
+}
---
+++ server/OnDisk.go
@@ -0,0 +1,72 @@
+package server
+
+import (
+	"bytes"
+	"errors"
+)
+
+type OnDisk struct {
+	buf    bytes.Buffer
+	resbuf bytes.Buffer
+}
+
+func NewOnDisk() (*OnDisk, error) {
+	return &OnDisk{}, nil
+}
+
+func (c *OnDisk) Send(msg []byte) error {
+	if len(msg) == 0 {
+		return errors.New("no content to send")
+	}
+	_, err := c.buf.Write(msg)
+	return err
+}
+
+func (c *OnDisk) Recv(buf []byte) ([]byte, error) {
+	if buf == nil {
+		buf = make([]byte, defaultBufferSize)
+	}
+	curbuf := buf
+	curLen := 0
+	if c.resbuf.Len() > 0 {
+		if c.resbuf.Len() > len(curbuf) {
+			return nil, errors.New("The buffer is too small to fit the message")
+		}
+		n, err := c.resbuf.Read(curbuf)
+		curLen = n
+		if err != nil {
+			return nil, err
+		}
+		c.resbuf.Reset()
+		curbuf = buf[n:]
+	}
+	n, err := c.buf.Read(curbuf)
+	if err != nil {
+		return nil, err
+	}
+
+	curLen += n
+	res := buf[0:curLen]
+
+	truncated, rest, err := cutLast(res)
+	if len(rest) > 0 {
+		_, err := c.resbuf.Write(rest)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return truncated, nil
+}
+
+func cutLast(buf []byte) (msg []byte, rest []byte, err error) {
+	n := len(buf)
+	if n == 0 || buf[n-1] == '\n' {
+		return buf, nil, nil
+	}
+
+	lastI := bytes.LastIndexByte(buf, '\n')
+	if lastI == -1 {
+		return nil, nil, errors.New("too small buffer")
+	}
+	return buf[:lastI+1], buf[lastI+1:], nil
+}
